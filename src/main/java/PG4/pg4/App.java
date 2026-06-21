e362b5b38b057a955246314c7d49b6ab132dce3c
Sudeeksha <poojarysudeeksha813@gmail.com>
2026-06-21 16:47:37 +0530

    first commit

---
+++ src/main/java/PG4/pg4/App.java
@@ -0,0 +1,13 @@
+package PG4.pg4;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World! Welcome to pgm4" );
+    }
+}
